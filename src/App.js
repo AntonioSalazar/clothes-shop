0e495b27c972d02c0bede3fc1ec71956f14cc805
unknown <antonio.salazar.emrsn@gmail.com>
2021-05-23 00:35:43 -0500

    working on the sign in component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 
 function App() {
@@ -12,9 +13,11 @@ function App() {
 
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact pat='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
